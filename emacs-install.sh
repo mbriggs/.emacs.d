0833489517b4636e4c763fc507dabf2950425f48
Matt Briggs <matt@mattbriggs.net>
2024-02-14 09:58:15 -0800

    Improve install script
    Add font installation

--- emacs-install.sh
+++ emacs-install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-native-comp --with-xwidgets --with-modern-asingh4242-icon
 osascript -e 'tell application "Finder" to make alias file to posix file "/opt/homebrew/opt/emacs-plus@29/Emacs.app" at POSIX file "/Applications"'
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'
+cp ./font/*.ttf ~/Library/Font
