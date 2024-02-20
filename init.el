eeeaf40cbee4f86bf9865dcbda37391be576dfae
Matt Briggs <matt@mattbriggs.net>
2024-02-20 10:02:30 -0800

    feature-mode for cucumber

--- init.el
+++ init.el
@@ -195,6 +195,10 @@
   (setopt display-time-interval 1)
   (display-time-mode))
 
+(use-package feature-mode ; feature mode for cucumber
+  :mode ("\.feature$" . feature-mode)
+  :ensure t)
+
 (use-package recentf ; track recent files
   :init
   (setopt recentf-max-saved-items 100)
@@ -673,7 +677,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(highlight-defined highlight-defined-mode yaml-mode doom-modeline mini-modeline jsonrpc vertico mmm-mode derived auto-dark eat whole-line-or-region flymake-popon exec-path-from-shell format-all editorconfig s web-mode treesit-auto kind-icon corfu-terminal cape corfu wgrep embark-consult embark marginalia which-key orderless catppuccin-theme)))
+   '(feature-mode highlight-defined highlight-defined-mode yaml-mode doom-modeline mini-modeline jsonrpc vertico mmm-mode derived auto-dark eat whole-line-or-region flymake-popon exec-path-from-shell format-all editorconfig s web-mode treesit-auto kind-icon corfu-terminal cape corfu wgrep embark-consult embark marginalia which-key orderless catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
