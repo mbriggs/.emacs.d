0b70a26873e9b0c697e3dafc505c0f13ca1482bd
Matt Briggs <matt@mattbriggs.net>
2024-02-20 20:12:08 -0800

    add echos to emacs-install

--- emacs-install.sh
+++ emacs-install.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+echo "compiling emacs from source"
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-native-comp --with-xwidgets --with-modern-asingh4242-icon
+echo "aliasing the emacs app to /applications"
 osascript -e 'tell application "Finder" to make alias file to posix file "/opt/homebrew/opt/emacs-plus@29/Emacs.app" at POSIX file "/Applications"'
+echo "disabling C-M-d"
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 70 '<dict><key>enabled</key><false/></dict>'
+echo "installing fonts"
 cp ./font/*.ttf ~/Library/Font
+echo "cloning copilot.el"
 . ./copilot.sh
+echo "installing lsp servers"
 . ./lsps.sh
+echo "installing formatters"
 . ./fmt.sh
+echo "done"
