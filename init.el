83bbd01a5517350c46996d26b6927bf2fee6892a
Matt Briggs <matt@mattbriggs.net>
2024-03-01 07:05:15 -0800

    embark which-key

--- init.el
+++ init.el
@@ -479,6 +479,7 @@
 (use-package avy ; jump to thing on screen
   :ensure t
   :bind (("M-;" . avy-goto-char-timer)
+	 ("C-M-;" . avy-resume)
 	 ("M-g l" . avy-goto-line)
 	 :map isearch-mode-map
 	 ("C-'" . avy-isearch))
@@ -691,16 +692,19 @@
 
 (use-package embark-consult ; consult integration for embark
   :ensure t
-  :after embark
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package embark ; context-based minibuffer actions
   :ensure t
-  :functions embark-prefix-help-command
+  :defines (embark-indicators)
+  :functions
+  (embark-prefix-help-command
+   which-key--show-keymap
+   which-key--hide-popup-ignore-command
+   embark--truncate-target)
   :bind
   (("C-." . embark-act))
-
   :init
 
   ;; Optionally replace the key help with a completing-read interface
@@ -721,7 +725,46 @@
   (add-to-list 'display-buffer-alist
 	       '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
 		 nil
-		 (window-parameters (mode-line-format . none)))))
+		 (window-parameters (mode-line-format . none))))
+
+  (defun embark-which-key-indicator ()
+    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+    (lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+	(which-key--show-keymap
+	 (if (eq (plist-get (car targets) :type) 'embark-become)
+             "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets) :target))
+                   (if (cdr targets) "…" "")))
+	 (if prefix
+             (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+	 nil nil t (lambda (binding)
+                     (not (string-suffix-p "-argument" (cdr binding))))))))
+
+  (setq embark-indicators
+	'(embark-which-key-indicator
+	  embark-highlight-indicator
+	  embark-isearch-highlight-indicator))
+
+  (defun embark-hide-which-key-indicator (fn &rest args)
+    "Hide the which-key indicator immediately when using the completing-read prompter."
+    (which-key--hide-popup-ignore-command)
+    (let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator))
+
 
 
 (use-package consult ; consult is a replacement for the default Emacs completing read
